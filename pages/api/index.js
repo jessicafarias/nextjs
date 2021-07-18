d4a5ccf1c584bf6ad136f47a570dbf7970edafd9
Jessica <jfarias@tribalworldwide.gt>
2021-07-17 19:32:12 -0500

    docs: Implement express next-connect and cors

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,8 +1,31 @@
 // pages/api/data.js
 // route => /api/data
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ message: 'hello' }))
-}
+// pages/api/data
+import nc from 'next-connect';
+import cors from 'cors'
+
+const handler = nc()
+  // use connect based middleware
+  .use(cors())
+  // express like routing for methods
+  .get((req, res) => {
+    res.json({ hello: 'world' })
+  })
+  .post((req, res) => {
+    res.send('Hello world')
+  })
+  .put(async (req, res) => {
+    res.end('hello')
+  })  
+  
+export default handler;
+
+// export default (req, res) => {
+//  res.statusCode = 200
+//  res.setHeader('Content-Type', 'application/json')
+//  res.end(JSON.stringify({ message: 'hello' }))
+//}
+// Add GET POST PATCH PUT DELETE
+// yarn add next-connect
+//  npm install cors || yarn add cors
